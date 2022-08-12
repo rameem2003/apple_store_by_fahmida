af3751ac1cbd8e7d7cfe875c0462ec136176dd9e
rameem2003 <rameem2019@gmail.com>
2022-08-12 21:21:29 +0600

    update

--- index.php
+++ index.php
@@ -6,13 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Apple Store || By Fahmida Yeasmin</title>
-    <!-- <link rel="shortcut icon" href="https://freepngimg.com/download/apple/58741-models-logo-apple-desktop-free-transparent-image-hq.png" type="image/x-icon"> -->
     <link rel="shortcut icon" href="./img/favicon.png" type="image/x-icon">
 
+    <!-- font awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    
+
+
     <!-- bootstrap 4 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
     
     <!-- local css -->
     <link rel="stylesheet" href="style.css">
